942fad30d8d377edba4b47e634a22ddf5497012d
jhkim93 <jhkim93@wemade.com>
2022-09-30 18:13:42 +0900

    fix: Todo앱을 class컴포넌트에서 functional 컴포넌트로 변경하기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-export default class App extends Component {
-  state = {
-    value: "",
-    todoData: [
-      {
-        id: "1",
-        title: "공부하기",
-        completed: true,
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        completed: false,
-      },
-      {
-        id: "3",
-        title: "요리하기",
-        completed: false,
-      },
-    ],
-  };
-  btnStyle = {
+export default function App() {
+  const [value, setValue] = useState("");
+  const [todoData, setTodoData] = useState([
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+    {
+      id: "3",
+      title: "요리하기",
+      completed: false,
+    },
+  ]);
+
+  const btnStyle = {
     color: "#fff",
     border: "none",
     padding: "5px 9px",
@@ -31,7 +30,7 @@ export default class App extends Component {
     float: "right",
   };
 
-  getStyle = (completed) => {
+  const getStyle = (completed) => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
@@ -39,14 +38,14 @@ export default class App extends Component {
     };
   };
 
-  getCompletedStyle = () => {
+  const getCompletedStyle = () => {
     return {
       padding: "10px",
       textDecoration: "line-through",
     };
   };
 
-  clickRemoveTodo = (ev) => {
+  const clickRemoveTodo = (ev) => {
     const check = window.confirm("삭제하시겠습니까?");
     if (!check) {
       return;
@@ -55,20 +54,20 @@ export default class App extends Component {
     const elem = ev.currentTarget;
     const id = elem.getAttribute("data-id");
 
-    let newTodoData = [...this.state.todoData.filter((v) => v.id !== id)];
-    this.setState({ todoData: newTodoData });
+    let newTodoData = [...todoData.filter((v) => v.id !== id)];
+    setTodoData(newTodoData);
   };
 
-  changeValue = (ev) => {
-    this.setState({ value: ev.target.value });
+  const changeValue = (ev) => {
+    setValue(ev.target.value);
   };
 
-  changeCompleteCheck = (id) => {
+  const changeCompleteCheck = (id) => {
     if (!id) {
       return;
     }
 
-    const newTodo = this.state.todoData.map((v) => {
+    const newTodo = todoData.map((v) => {
       return id !== v.id
         ? v
         : {
@@ -78,74 +77,72 @@ export default class App extends Component {
           };
     });
 
-    this.setState({ todoData: newTodo });
+    setTodoData(newTodo);
   };
 
-  submitForm = (ev) => {
+  const submitForm = (ev) => {
     ev.preventDefault();
-    if (this.state.value.length < 1) {
+    if (value.length < 1) {
       alert("할 일을 입력하세요");
       return;
     }
 
     const newTodo = {
       id: Date.now().toString(),
-      title: this.state.value,
+      title: value,
       completed: false,
     };
 
     console.log(newTodo);
-    // const newTodoData = this.state.todoData.push(newTodo);
-    this.setState({
-      todoData: [...this.state.todoData, newTodo],
-      value: "",
-    });
+    // const newTodoData = todoData.push(newTodo);
+
+    // setTodoData([...todoData, newTodo]);
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue("");
   };
 
-  render() {
-    return (
-      <div className="container">
-        <div className="todoBlock">
-          <div className="title">
-            <h1>할 일 목록</h1>
-          </div>
-
-          {this.state.todoData.map((v) => (
-            <div style={this.getStyle(v.completed)} key={v.id}>
-              <label>
-                <input
-                  type="checkbox"
-                  defaultChecked={v.completed}
-                  onChange={() => this.changeCompleteCheck(v.id)}
-                />
-                {v.title}
-              </label>
-              <button
-                type="button"
-                style={this.btnStyle}
-                data-id={v.id}
-                onClick={this.clickRemoveTodo}
-              >
-                X
-              </button>
-            </div>
-          ))}
-
-          <form style={{ display: "flex" }} onSubmit={this.submitForm}>
-            <input
-              type="text"
-              name="value"
-              style={{ flex: "10", padding: "5px" }}
-              placeholder="해야 할 일을 입력하세요"
-              value={this.state.value}
-              onChange={this.changeValue}
-            />
-            <button type="submit" className="btn" style={{ flex: "1" }}>
-              추가
-            </button>
-          </form>
+  return (
+    <div className="container">
+      <div className="todoBlock">
+        <div className="title">
+          <h1>할 일 목록</h1>
         </div>
+
+        {todoData.map((v) => (
+          <div style={getStyle(v.completed)} key={v.id}>
+            <label>
+              <input
+                type="checkbox"
+                defaultChecked={v.completed}
+                onChange={() => changeCompleteCheck(v.id)}
+              />
+              {v.title}
+            </label>
+            <button
+              type="button"
+              style={btnStyle}
+              data-id={v.id}
+              onClick={clickRemoveTodo}
+            >
+              X
+            </button>
+          </div>
+        ))}
+
+        <form style={{ display: "flex" }} onSubmit={submitForm}>
+          <input
+            type="text"
+            name="value"
+            style={{ flex: "10", padding: "5px" }}
+            placeholder="해야 할 일을 입력하세요"
+            value={value}
+            onChange={changeValue}
+          />
+          <button type="submit" className="btn" style={{ flex: "1" }}>
+            추가
+          </button>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
